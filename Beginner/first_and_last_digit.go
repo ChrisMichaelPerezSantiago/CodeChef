8e6cdeef020eac3947577c3ff1de80a5878dba12
Chris M <chris.perez@upr.edu>
2017-05-12 16:54:36 -0400

    Update first_and_last_digit.go

--- Beginner/first_and_last_digit.go
+++ Beginner/first_and_last_digit.go
@@ -1,33 +1,32 @@
 package main
-
 import (
-	"fmt"
-	"strconv"
+   "fmt"
+   "strconv"
 )
 
 /*
-	Problem: First and Last Digit
-	Problem Code: FLOW004
+    Problem: First and Last Digit
+    Problem Code: FLOW004
 
-	@Author:  Chris M. Perez
-	@Date:    5/12/2017
+    @Author:  Chris M. Perez
+    @Date:    5/12/2017
 */
 
 func sum(str string) (total int) {
-	start, _ := strconv.Atoi(string([]rune(str)[0]))
-	last, _ := strconv.Atoi(string([]rune(str)[len(str)-1]))
+   start, _ := strconv.Atoi(string([]rune(str)[0]))
+   last, _ := strconv.Atoi(string([]rune(str)[len(str)-1]))
 
-	total = start + last
-	return total
+   total = start + last
+   return total
 }
 
 func main() {
-	var n int
-	var str string
-
-	fmt.Scan(&n)
-	for index := 0; index < n; index++ {
-		fmt.Scan(&str)
-		fmt.Println(sum(str))
-	}
+   var n int
+   var str string
+  
+   fmt.Scan(&n)
+   for index := 0; index < n; index++ {
+      fmt.Scan(&str)
+      fmt.Println(sum(str))
+   }
 }
