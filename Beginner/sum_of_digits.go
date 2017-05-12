3688b3e88bba4f94a9f78a946661ef1ae5a7e0d7
Chris M <chris.perez@upr.edu>
2017-05-12 00:29:19 -0400

    Update sum_of_digits.go

--- Beginner/sum_of_digits.go
+++ Beginner/sum_of_digits.go
@@ -3,34 +3,35 @@ package main
 import "fmt"
 
 /*
-	Problem: Sum of Digits
-	Problem Code: FLOW006
+   Problem: Sum of Digits
+   Problem Code: FLOW006
 
-	@Author:  Chris M. Perez
-	@Date:    5/12/2017
+   @Author:  Chris M. Perez
+   @Date:    5/12/2017
 */
 
 func sumDigit(args int) (total int) {
-	n := args
-
-	for n != 0 {
-		total += n % 10
-		n /= 10
-	}
-	return total
+   n := args
+	
+   for n != 0 {
+      total += n % 10
+        n /= 10
+   }
+   return total
 }
 
 func exe(n int) {
-	var args int
-	for index := 0; index < n; index++ {
-		fmt.Scan(&args)
-		fmt.Println(sumDigit(args))
-	}
+   var args int
+	
+   for index := 0; index < n; index++ {
+     fmt.Scan(&args)
+     fmt.Println(sumDigit(args))
+   }
 }
 
 func main() {
-	var n int
+   var n int
 
-	fmt.Scan(&n)
-	exe(n)
+   fmt.Scan(&n)
+   exe(n)
 }
